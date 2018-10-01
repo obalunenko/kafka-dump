9d2618c4d4d7b86c881c713bee1ff061fc90c12a
Oleg Balunenko <oleg.balunenko@gmail.com>
2018-10-02 01:38:13 +0300

    add version stdout

--- main.go
+++ main.go
@@ -1,11 +1,22 @@
 package main
 
 import (
+	"fmt"
+
 	"github.com/oleg-balunenko/kafka-dump/config"
 	"github.com/oleg-balunenko/kafka-dump/dumper"
 )
 
+var (
+	version string
+	build   string
+	commit  string
+)
+
 func main() {
+
+	fmt.Printf("Version info: %s:%s", version, build)
+	fmt.Printf("commit: %s ", commit)
 
 	service := config.LoadConfig()
 
