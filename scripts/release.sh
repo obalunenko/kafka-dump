eed88d6c788736073a08c449d2b7da1e08723238
Oleg Balunenko <obalunenko@partners.luno.com>
2018-12-25 16:18:11 +0300

    fix codefactor issues

--- scripts/release.sh
+++ scripts/release.sh
@@ -4,7 +4,7 @@
 git fetch --tags
 
 # Get the latest tag name
-latestTag=$(git describe --tags `git rev-list --tags --max-count=1`)
+latestTag=$(git describe --tags $(git rev-list --tags --max-count=1))
 echo ${latestTag}
 
 curl -sL https://git.io/goreleaser | bash
