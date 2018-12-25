8dea1a8640c5455ae7d205c0ef1d9cf04c34ded9
Oleg Balunenko <obalunenko@partners.luno.com>
2018-12-25 16:05:40 +0300

    main: fix linter warnings use date var for ldflags

--- main.go
+++ main.go
@@ -9,17 +9,18 @@ import (
 
 var (
 	version string
-	build   string
+	date    string
 	commit  string
 )
 
 func main() {
 
-	fmt.Printf("Version info: %s:%s\n", version, build)
+	fmt.Printf("Version info: %s:%s\n", version, date)
 	fmt.Printf("commit: %s \n", commit)
 
 	svcCfg := config.LoadConfig()
 
-	dumper.Start(svcCfg.KafkaBrokers, svcCfg.KafkaGroupID, svcCfg.KafkaClientID, svcCfg.KafkaVersion(), svcCfg.Newest, svcCfg.Topics, svcCfg.OutputDir)
+	dumper.Start(svcCfg.KafkaBrokers, svcCfg.KafkaGroupID, svcCfg.KafkaClientID,
+		svcCfg.KafkaVersion(), svcCfg.Newest, svcCfg.Topics, svcCfg.OutputDir)
 
 }
